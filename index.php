73da668f5dd1246cb42622906d8647370e3702d8
Eka Nur Ahmad R <Eka Nur Ahmad R>
2016-04-05 06:20:10 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> Invitania.com </title>
+
+    <meta property="fb:app_id" content="302184056577324" /> 
+    <meta property="og:type" content="website" /> 
+    <meta property="og:url" content="http://invitania.com/" />
+    <meta property="og:image" content="http://invitania.com/img/invitania.gif" />
+    <meta property="og:title" content="Video Undangan Online" />
+    <meta property="og:description" content="Meraih kemenangan bersama, jadikan moment ini istimewa bersama invitania" />
+
+    <meta name="description" content="Video Undangan Online" />
+<!--     <meta name="google-site-verification" content="Video Undangan Online" />
+    <meta name="author" content="By Hannorafy">
+    <meta name="robots" content="NOODP">
+    <meta name="googlebot" content="NOODP">
+    <meta name="googlebot" content="NOODP, nofollow"> -->
+
+    
+    
+    <!-- Mobile Specific Meta -->
+    <meta name="viewport"content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link href='https://fonts.googleapis.com/css?family=Medula+One' rel='stylesheet' type='text/css'>
+    <style type="text/css">
+    	body{
+    		background-color: #F10;
+    		font-family: 'Medula One', cursive;
+    		color: #FFFFD6;
+    	}
+
+    	h1{
+    		margin-top: 25px;
+    		text-align: center;
+    		font-size: 200%;
+    		letter-spacing: 4px;
+    		font-weight: normal;
+    	}
+
+    	h3{
+    		text-align: center;
+    		font-size: 200%;
+    		letter-spacing: 4px;
+    		font-weight: normal;
+            margin-bottom: 0.1em;
+    	}
+
+        h4{
+            text-align: center;
+            font-size: 200%;
+            letter-spacing: 4px;
+            font-weight: normal;
+            margin-bottom: 0.1em;
+            margin-top: 0.5em;
+        }
+
+
+    	.image{
+    		margin-top: 10px;
+    		text-align: center;
+    	}
+
+        img {
+            max-width: 100%;
+            height: auto;
+        }
+    </style>
+</head>
+<body>
+	<br>
+	<br>
+	<br>
+	<h1>We are currently working on <br>something awesome. Stay tuned ! </h1>
+	<div class="image">
+		<img src="img/invitania.gif">
+	</div>
+	<h3>Be Patient. Love You! </h3>
+    <br>
+    <h4>~H.G~</h4>
+</body>
+</html>
